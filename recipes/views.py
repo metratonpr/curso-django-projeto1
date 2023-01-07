6c0882caa702ce8b86a7c5b79703afdde5b57164
Juliano Roque Vieira <julianovieira@yahoo.com.br>
2023-01-07 13:47:45 -0300

    Details and filters

--- recipes/views.py
+++ recipes/views.py
@@ -2,9 +2,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 # Create your views here.
 
 
 def home(request):
     # return http request
-    return render(request, 'recipes/pages/home.html', status=200, context={'name': 'Juliano', })
+    return render(request, 'recipes/pages/home.html', status=200, context={'recipes': [make_recipe() for _ in range(10)], })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', status=200, context={'recipe': make_recipe(), 'is_detail_page': True})
