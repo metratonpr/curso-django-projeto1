f55a2d24f76845d455520516ca719a084e33d5f8
Juliano Roque Vieira <julianovieira@yahoo.com.br>
2023-01-11 22:32:50 -0300

    New Instalation

--- projeto/settings.py
+++ projeto/settings.py
@@ -125,6 +125,11 @@ STATICFILES_DIRS = [
     BASE_DIR / "base_static",
 ]
 
+MEDIA_ROOT = BASE_DIR / 'media/'
+
+MEDIA_URL = '/media/'
+
+
 STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
