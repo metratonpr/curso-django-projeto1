246a063e479d3933a4683731f7582c504f068bbf
Juliano Roque Vieira <julianovieira@yahoo.com.br>
2023-02-04 12:26:06 -0300

    start 0402

--- recipes/views.py
+++ recipes/views.py
@@ -4,13 +4,22 @@ from django.shortcuts import render
 
 from utils.recipes.factory import make_recipe
 
+from .models import Recipe
+
 # Create your views here.
 
 
 def home(request):
     # return http request
-    return render(request, 'recipes/pages/home.html', status=200, context={'recipes': [make_recipe() for _ in range(10)], })
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    return render(request, 'recipes/pages/home.html', status=200, context={'recipes': recipes})
 
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/recipe-view.html', status=200, context={'recipe': make_recipe(), 'is_detail_page': True})
+    recipe = Recipe.objects.filter(id=id, is_published=True).first()
+    return render(request, 'recipes/pages/recipe-view.html', status=200, context={'recipe': recipe, 'is_detail_page': True})
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id).order_by('-id')
+    return render(request, 'recipes/pages/category.html', status=200, context={'recipes': recipes})
