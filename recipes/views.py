eb1ef41120b6503f4d47258343c6ac7afb76382a
Juliano Roque Vieira <julianovieira@yahoo.com.br>
2023-08-08 10:00:47 -0300

    Restaurante-Django

--- recipes/views.py
+++ recipes/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import get_list_or_404, get_object_or_404, render
 
-from .models import Recipe
+from recipes.models import Recipe
 
 # Create your views here.
 
 
 def home(request):
-    # return http request
+    # return http request41
     recipes = get_list_or_404(Recipe.objects.filter(
         is_published=True).order_by('-id'))
-    return render(request, 'recipes/pages/home.html', status=200, context={'recipes': recipes})
+    return render(request, 'recipes/pages/home.html',
+    context={'recipes': recipes})  # noqa
 
 
 def recipe(request, id):
     recipe = get_object_or_404(Recipe.objects.filter(
-        pk=id, is_published=True).first())
+        pk=id, is_published=True))
 
-    return render(request, 'recipes/pages/recipe-view.html', status=200, context={'recipe': recipe, 'is_detail_page': True})
+    return render(request, 'recipes/pages/recipe-view.html',
+    context={'recipe': recipe, 'is_detail_page': True})  # noqa
 
 
 def category(request, category_id):
@@ -25,4 +27,5 @@ def category(request, category_id):
     #     raise Http404('Not Found')
     recipes = get_list_or_404(Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id'))
-    return render(request, 'recipes/pages/category.html', status=200, context={'recipes': recipes, 'title': f'{recipes[0].category.name}  - Category | '})
+    return render(request, 'recipes/pages/category.html',
+    context={'recipes': recipes, 'title': f'{recipes[0].category.name}  - Category | '})  # noqa
